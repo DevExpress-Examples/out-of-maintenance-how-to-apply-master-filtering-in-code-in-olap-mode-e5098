6a5b3f2dc6e59dbccd79582aeb10edda23a8253f
DevExpressExampleBot <fakeEmail>
2022-06-17 20:32:20 +0400

    Source auto update [skip ci]

--- VB/Dashboard_SetMasterFilter_OLAP/Form1.vb
+++ VB/Dashboard_SetMasterFilter_OLAP/Form1.vb
@@ -1,24 +1,26 @@
-Imports System.Collections.Generic
+﻿Imports System.Collections.Generic
+Imports System.Windows.Forms
 Imports System.Collections
 
 Namespace Dashboard_SetMasterFilter_OLAP
+	Partial Public Class Form1
+		Inherits DevExpress.XtraEditors.XtraForm
 
-    Public Partial Class Form1
-        Inherits DevExpress.XtraEditors.XtraForm
+		Public Sub New()
+			InitializeComponent()
+			' Loads a dashboard from an XML file.
+			dashboardViewer1.LoadDashboard("..\..\Data\Dashboard.xml")
 
-        Public Sub New()
-            InitializeComponent()
-            ' Loads a dashboard from an XML file.
-            dashboardViewer1.LoadDashboard("..\..\Data\Dashboard.xml")
-            ' Creates two sets of OLAP members' unique names to select corresponding cards 
-            ' in the Card dashboard item.
-            Dim cardValues1 As List(Of Object) = New List(Of Object)()
-            cardValues1.AddRange({"[Date].[Calendar Year].&[2001]", "[Date].[Calendar Quarter of Year].&[CY Q4]"})
-            Dim cardValues2 As List(Of Object) = New List(Of Object)()
-            cardValues2.AddRange({"[Date].[Calendar Year].&[2002]", "[Date].[Calendar Quarter of Year].&[CY Q1]"})
-            Dim selectedCards As List(Of IList) = New List(Of IList)({cardValues1, cardValues2})
-            ' Selects specified cards in the Card dashboard item.
-            dashboardViewer1.SetMasterFilter("cardDashboardItem1", selectedCards)
-        End Sub
-    End Class
+			' Creates two sets of OLAP members' unique names to select corresponding cards 
+			' in the Card dashboard item.
+			Dim cardValues1 As New List(Of Object)()
+			cardValues1.AddRange( { "[Date].[Calendar Year].&[2001]", "[Date].[Calendar Quarter of Year].&[CY Q4]" })
+			Dim cardValues2 As New List(Of Object)()
+			cardValues2.AddRange( { "[Date].[Calendar Year].&[2002]", "[Date].[Calendar Quarter of Year].&[CY Q1]" })
+			Dim selectedCards As New List(Of IList)( { cardValues1, cardValues2 })
+
+			' Selects specified cards in the Card dashboard item.
+			dashboardViewer1.SetMasterFilter("cardDashboardItem1", selectedCards)
+		End Sub
+	End Class
 End Namespace
